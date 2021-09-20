8a094ae94bf54fbad5fbfc40bdccb52aa5f4fbfa
buhe <hare.bu@herewhite.com>
2021-09-20 22:29:36 +0800

    bump 0.2

--- src/interface/tui.rs
+++ src/interface/tui.rs
@@ -1,5 +1,5 @@
-use crate::sdk::Story;
-use cursive::views::{TextView,ListView};
+use crate::{next, prev, sdk::Story};
+use cursive::{Cursive, view::SizeConstraint, views::{Button, DummyView, LinearLayout, ListView, ResizedView, TextView}};
 
 pub fn show(items: Vec<Story>) {
      let mut siv = cursive::default();
@@ -9,7 +9,30 @@ pub fn show(items: Vec<Story>) {
         l.add_child(&s.id.to_string(),TextView::new(&s.title));
     }
     // Creates a dialog with a single "Quit" button
+   
+    //   let select = SelectView::<String>::new()
+    //     // .on_submit(on_submit)
+    //     // .with_name("select")
+    //     .fixed_size((10, 5));
+    let buttons = LinearLayout::horizontal()
+        .child(Button::new("Prev", prev))
+        .child(Button::new("Next", next))
+        .child(DummyView)
+        .child(Button::new("Quit", Cursive::quit));
+        let buttons2 = LinearLayout::vertical().child(buttons);
+    l.add_child("Select",ResizedView::new(SizeConstraint::Fixed(20),
+                                SizeConstraint::Fixed(1),
+                                buttons2));
     siv.add_layer(l);
+    // siv.add_layer(Dialog::around(LinearLayout::vertical()
+    //         // .child(select)
+    //         .child(DummyView)
+    //         .child(DummyView)
+    //         .child(DummyView)
+    //         .child(buttons))
+    //     .title("Select a profile"));
+
     // Starts the event loop.
+    siv.add_global_callback('q', |s| s.quit());
     siv.run();
 } 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
 
 
+use std::{error, sync::Mutex};
+
+use cursive::Cursive;
 use sdk::Story;
 
 use crate::sdk::{get_item, get_items};
 mod sdk;
 mod interface;
+const PAGE_SIZE: usize = 10;
+ #[macro_use] 
+ extern crate lazy_static;
+ lazy_static! {
+   static ref INDEX: Mutex<usize> = Mutex::new(0);
+ }
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = get_items(0,10).await?;
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    let resp = get_items(*INDEX.lock().unwrap(),PAGE_SIZE).await?;
     let mut ss = Vec::<Story>::new();
       for i in 0..resp.len() - 1 {
         ss.push(get_item(resp[i]).await?);
       }
     interface::tui::show(ss);
     Ok(())
+}
+
+pub fn next(_c: &mut Cursive) {
+    *INDEX.lock().unwrap() += 10;
+}
+pub fn prev(_c: &mut Cursive) {
 }
