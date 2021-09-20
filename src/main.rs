f326019d00d4b6f9a2c006ca9e94a86c37aa2711
buhe <hare.bu@herewhite.com>
2021-09-20 09:44:58 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
+    let resp = reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json")
         .await?
-        .json::<HashMap<String, String>>()
+        .json::<Vec<u32>>()
         .await?;
     println!("{:#?}", resp);
     // Creates the cursive root - required for every application.
-    let mut siv = cursive::default();
-    let mut l = ListView::new();
-    l.add_child("1",TextView::new("2"));
-    // Creates a dialog with a single "Quit" button
-    siv.add_layer(l);
+    // let mut siv = cursive::default();
+    // let mut l = ListView::new();
+    // l.add_child("1",TextView::new("2"));
+    // // Creates a dialog with a single "Quit" button
+    // siv.add_layer(l);
 
-    // Starts the event loop.
-    siv.run();
+    // // Starts the event loop.
+    // siv.run();
     Ok(())
 }
