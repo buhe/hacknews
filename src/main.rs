039f944192b777a4bf68170ec85bd9eae5ce9307
buhe <hare.bu@herewhite.com>
2021-09-20 14:52:28 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // l.add_child("1",TextView::new("2"));
     // // Creates a dialog with a single "Quit" button
     // siv.add_layer(l);
-
     // // Starts the event loop.
     // siv.run();
     Ok(())
